82ad0b6f06998253fa0a5bf26b4ed445ae29ced0
Martin Tausen <martausen@gmail.com>
2022-04-04 00:42:43 +0200

    avg + current time + new thresholds for colors

--- src/App.vue
+++ src/App.vue
@@ -3,15 +3,18 @@ import { ref, onMounted, computed } from 'vue'
 import colors from 'tailwindcss/colors'
 import Chart from 'chart.js/auto';
 import 'chartjs-adapter-dayjs-3';
+import annotationPlugin from 'chartjs-plugin-annotation';
 import { record } from './types'
-import { getData } from './util'
+import { getData, get30DayAvg } from './util'
 import dayjs from 'dayjs';
 import 'dayjs/locale/da'
+Chart.register(annotationPlugin);
 dayjs.locale('da')
 // https://n1.dk/priser-og-vilkaar
 const lavlast: number = 20.38
 const spidslast: number = 62.66
 const records = ref<record[] | null | undefined>(undefined)
+const avg30days = ref<number>()
 // SpotPriceDKK er i DKK/MWh og SpotPriceDKK er null i weekenden og omregnes derfor fra SpotPriceEUR
 // Der omregnet her til øre/KWh
 const prisUdenAfgift = (record: record): number => record.SpotPriceDKK / 1_000 * 100 || record.SpotPriceEUR * 7.44 / 1_000 * 100
@@ -23,7 +26,6 @@ const prisMedAfgift = (record: record): number => (record.HourDK.month() >= 10 -
 const priserUdenAfgift = computed<number[]>(() => records.value.map(item => prisUdenAfgift(item)))
 const priserMedAfgift = computed<number[]>(() => records.value.map(item => prisMedAfgift(item)))
 const afgifter = computed<number[]>(() => records.value.map(item => prisMedAfgift(item) - prisUdenAfgift(item)))
-const avg = (array: number[]): number => array.reduce((previousValue, currentValue) => (previousValue + currentValue / array.length), 0);
 const chartRef = ref(null)
 const drawChart = () => {
   new Chart(chartRef.value, {
@@ -41,11 +43,39 @@ const drawChart = () => {
               hour: "HH"
             }
           }
+        },
+        y: {
+          stacked: true,
+          suggestedMax: 200
         }
       },
       plugins: {
         legend: {
           display: false
+        },
+        annotation: {
+          annotations: {
+            currentTime: {
+              type: 'line',
+              borderColor: colors.neutral[400],
+              borderDash: [6, 6],
+              borderWidth: 1,
+              xScaleID: 'x',
+              xMin: dayjs().startOf("hour").valueOf(),
+              xMax: dayjs().startOf("hour").valueOf(),
+              yMin: 0,
+              yMax: 1000,
+              yScaleID: 'y'
+            },
+            avgPrice: {
+              type: 'line',
+              borderColor: colors.blue[500],
+              borderDash: [6, 6],
+              borderWidth: 1,
+              scaleID: 'y',
+              value: avg30days.value
+            }
+          }
         }
       }
     },
@@ -53,24 +83,14 @@ const drawChart = () => {
       labels: records.value.map(item => item.HourDK.toDate()),
       datasets: [
         {
-          type: "line",
-          pointRadius: 0,
-          borderWidth: 2,
-          label: "Gennemsnit",
-          data: [undefined, undefined, ...records.value.map(() => avg(priserMedAfgift.value))],
-          fill: false,
-          borderColor: colors.blue[800],
-        },
-        {
           type: "bar",
-          label: "Afgift",
+          label: "N1 A/S Afgift",
           data: afgifter.value,
           backgroundColor: records.value.map(item => {
-            if (item.HourDK.isSame(dayjs(), "hour")) return colors.neutral[500]
-            else if (prisMedAfgift(item) == Math.min(...priserMedAfgift.value.slice(2))) return colors.green[600]
+            if (prisMedAfgift(item) == Math.min(...priserMedAfgift.value.slice(2))) return colors.green[600]
             else if (prisMedAfgift(item) == Math.max(...priserMedAfgift.value.slice(2))) return colors.orange[800]
-            else if (prisMedAfgift(item) < 100) return colors.green[700]
-            else if (prisMedAfgift(item) > 200) return colors.orange[900]
+            else if (prisUdenAfgift(item) < avg30days.value * 0.5) return colors.green[700]
+            else if (prisUdenAfgift(item) > avg30days.value * 1.2) return colors.orange[900]
             else return colors.neutral[700]
           })
         },
@@ -83,11 +103,10 @@ const drawChart = () => {
           // }),
           data: priserUdenAfgift.value,
           backgroundColor: records.value.map(item => {
-            if (item.HourDK.isSame(dayjs(), "hour")) return colors.neutral[400]
-            else if (prisMedAfgift(item) == Math.min(...priserMedAfgift.value.slice(2))) return colors.green[500]
+            if (prisMedAfgift(item) == Math.min(...priserMedAfgift.value.slice(2))) return colors.green[500]
             else if (prisMedAfgift(item) == Math.max(...priserMedAfgift.value.slice(2))) return colors.orange[700]
-            else if (prisMedAfgift(item) < 100) return colors.green[600]
-            else if (prisMedAfgift(item) > 200) return colors.orange[800]
+            else if (prisUdenAfgift(item) < avg30days.value * 0.5) return colors.green[700]
+            else if (prisUdenAfgift(item) > avg30days.value * 1.2) return colors.orange[900]
             else return colors.neutral[600]
           })
         }
@@ -97,9 +116,11 @@ const drawChart = () => {
 }
 onMounted(async () => {
   // if (dayjs().isAfter(lo))
-  records.value = await getData()
+  records.value = await getData();
+  avg30days.value = await get30DayAvg();
+  console.log(avg30days.value);
   
-  drawChart()
+  drawChart();
 })
 </script>
 <template>
@@ -107,27 +128,27 @@ onMounted(async () => {
     <div v-if="records === undefined" class="animate-pulse">Henter data...</div>
     <div v-if="records === null">Fejl.</div>
   </div>
-  <canvas ref="chartRef"></canvas>
-  <div class="p-4 text-sm flex space-x-8 space-y-2 items-center justify-center flex-wrap">
-    <div class="flex items-center space-x-2">
+  <canvas class ref="chartRef"></canvas>
+  <div class="p-4 text-sm flex gap-x-8 gap-y-2 items-center justify-center flex-wrap">
+    <div class="flex items-center gap-x-2">
       <div class="bg-green-500 w-4 h-4"></div>
-      <div>Billigst i perioden</div>
+      <div>Billigst</div>
     </div>
-    <div class="flex items-center space-x-2">
+    <div class="flex items-center gap-x-2">
       <div class="bg-green-600 w-4 h-4"></div>
-      <div>Under 100 øre</div>
+      <div>50% under gns.</div>
     </div>
-    <div class="flex items-center space-x-2">
+    <div class="flex items-center gap-x-2">
       <div class="bg-orange-700 w-4 h-4"></div>
-      <div>Over 200 øre</div>
+      <div>20% over gns.</div>
     </div>
-    <div class="flex items-center space-x-2">
+    <div class="flex items-center gap-x-2">
       <div class="bg-orange-600 w-4 h-4"></div>
-      <div>Dyrest perioden</div>
+      <div>Dyrest</div>
     </div>
-    <div class="flex items-center space-x-2">
+    <div class="flex items-center gap-x-2">
       <div class="bg-blue-800 w-4 h-4"></div>
-      <div>Gennemsnit i perioden</div>
+      <div>30 dages gns.</div>
     </div>
   </div>
 </template>
--- src/util.ts
+++ src/util.ts
@@ -24,11 +24,26 @@ export const getData = async (): Promise<record[]> => new Promise((resolve, reje
       .catch((e) => {
         reject(e)
       })
-    // resolve(sampledata.elspotprices.map(item => {
-    //   return {
-    //     HourDK: dayjs(item.HourDK),
-    //     SpotPriceDKK: item.SpotPriceDKK,
-    //     SpotPriceEUR: item.SpotPriceEUR,
-    //   }
-    // }))
+  })
+
+  export const avg = (array: number[]): number => array.reduce((previousValue, currentValue) => (previousValue + currentValue / array.length), 0);
+
+  export const get30DayAvg = async (): Promise<number> => new Promise((resolve, reject) => {
+    const data = { "query": `query Dataset {elspotprices(where: {HourDK: {_gte: \"${dayjs().subtract(30, "days").format("YYYY-MM-DDTHH:mm")}\"}PriceArea: {_eq: \"DK1\"}} order_by: {HourDK: asc} limit: 1000){HourDK SpotPriceDKK SpotPriceEUR}}` }
+    fetch(`https://data-api.energidataservice.dk/v1/graphql`,
+      {
+        method: "POST",
+        body: JSON.stringify(data),
+        headers: {
+          "Content-Type": "application/json"
+        }
+      })
+      .then(raw => raw.json())
+      .then(json => json.data.elspotprices as record[])
+      .then(data => {
+        resolve(avg(data.map(item => item.SpotPriceEUR)) * 7.44 / 1000 * 100)
+      })
+      .catch((e) => {
+        reject(e)
+      })
   })
