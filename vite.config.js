11836425ece13f10c85d941489c05c8d16d6e644
Martin Tausen <martausen@gmail.com>
2022-07-07 22:08:40 +0200

    test

--- vite.config.js
+++ vite.config.js
@@ -4,15 +4,16 @@ import fs from 'fs'
 
 // https://vitejs.dev/config/
 export default ({ mode }) => {
+  console.log(process.env);
   process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
   return defineConfig({
     plugins: [vue()],
     server: {
       host: process.env.VITE_LOCAL_HOST,
-      https: {
+      https: process.env.NODE_ENV !== "development" ? {
         key: fs.readFileSync("../" + process.env.VITE_LOCAL_HOST + "-key.pem"),
         cert: fs.readFileSync("../" + process.env.VITE_LOCAL_HOST + ".pem")
-      }
+      } : {}
     }
   })
 }
