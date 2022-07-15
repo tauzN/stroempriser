fb4f05adcfe6cc6bbaefadbac0f18f2bbc385da2
Martin Tausen <martausen@gmail.com>
2022-07-15 23:14:48 +0200

    cache

--- src/Router.vue
+++ src/Router.vue
@@ -1,11 +1,14 @@
 <template>
-  <router-view>
+  <router-view v-slot="{Component}">
+    <KeepAlive>
+      <component :is="Component" />
+    </KeepAlive>
   </router-view>
   <div class=" fixed bottom-0 left-0 bg-gray-800 w-full h-10 flex items-center justify-evenly">
     <router-link to="/">Priser</router-link>
     <router-link to="/elbil">Opladning</router-link>
   </div>
-    <div class="h-10"></div>
+  <div class="h-10"></div>
 </template>
 <style scoped>
 a {
