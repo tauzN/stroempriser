425bff8ce6ba7fdb97ce1b4a6c5b2c21090b2462
Martin Tausen <martausen@gmail.com>
2022-07-17 13:04:11 +0200

    aler on error + lint

--- src/util.ts
+++ src/util.ts
@@ -22,33 +22,34 @@ const DKKMWh_to_DKKkWh = (DKK: number) => DKK / 1_000;
 export const getLastDays = async (days: number): Promise<record[]> =>
   new Promise((resolve, reject) => {
     const filter = {
-      "PriceArea": "DK1"
-    }
+      PriceArea: "DK1",
+    };
     const params = new URLSearchParams({
-      "filter": JSON.stringify(filter),
-      "limit": (24 * days).toString()
-    })
-    fetch("https://api.energidataservice.dk/dataset/Elspotprices?"+params)
-    .then((raw) => raw.json())
-    .then((json) => json.records as rawRecord[])
-    .then((data) => {      
-      let prices = data.map((item) => {
-        return {
-          // SpotPriceDKK er null i weekenden og omregnes derfor fra SpotPriceEUR
-          price:
-            Math.round(
-              DKKMWh_to_DKKkWh(
-                item.SpotPriceDKK || item.SpotPriceEUR * 7.44
-              ) *
-                1.25 *
-                100
-            ) / 100,
-          datetime: dayjs(item.HourDK).toDate(),
-        };
-      });
-      resolve(prices.reverse());
-    })
-    .catch((e) => {
-      reject(e);
+      filter: JSON.stringify(filter),
+      limit: (24 * days).toString(),
     });
+    fetch("https://api.energidataservice.dk/dataset/Elspotprices?" + params)
+      .then((raw) => raw.json())
+      .then((json) => json.records as rawRecord[])
+      .then((data) => {
+        let prices : record[] = data.map((item) => {
+          return {
+            // SpotPriceDKK er null i weekenden og omregnes derfor fra SpotPriceEUR
+            price:
+              Math.round(
+                DKKMWh_to_DKKkWh(
+                  item.SpotPriceDKK || item.SpotPriceEUR * 7.44
+                ) *
+                  1.25 *
+                  100
+              ) / 100,
+            datetime: dayjs(item.HourDK).toDate(),
+          };
+        });
+        resolve(prices.reverse());
+      })
+      .catch((e) => {
+        alert(e);
+        reject(e);
+      });
   });
