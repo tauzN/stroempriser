f9863835ec217d6c5c0511958adeeebe0cd4f2f9
Martin Tausen <martausen@gmail.com>
2022-09-22 23:14:41 +0200

    colors

--- src/Elpriser.vue
+++ src/Elpriser.vue
@@ -1,28 +1,30 @@
 <template>
-    <div class=" text-[10px] p-2 font-mono">
+    <div class=" text-xs p-2 font-mono">
         <div v-show="!prices?.length" class=" animate-pulse">Henter data...</div>
         <template v-for="item in last36Hours">
             <div class="w-full flex items-center justify-center">
                 <div class="w-8 flex items-center justify-center z-40">
                     <div class="">{{dayjs(item.datetime).format("HH")}}</div>
                 </div>
-                <div class="w-full py-[1px] z-30">
-                    <div class=" rounded-full border pl-2 flex  justify-between"
-                        :style="`border-color: ${barColor(item.price)}; ` + `width: ${barLength(item.price)}%`">
+                <div class="w-full z-30">
+                    <div class=" rounded-r-full border-r-2 pl-2 flex  justify-between"
+                        :style="`border-color: hsl(${barColorHue(item.price)} 60% 40%);
+                        width: ${barLength(item.price)}%;
+                        background-color: hsla(${barColorHue(item.price)}, 60%, 40%, .3);
+                        
+                        `">
                         <span>
                             {{(afgifter + item.price).toLocaleString("da-dk",
                             {minimumFractionDigits:2, maximumFractionDigits:2})}}
                         </span>
-                        <span v-if="item.price === min(last36Hours.map(item => item.price))"
-                            class="pr-2">Billigst</span>
-                        <span v-if="item.price === max(last36Hours.map(item => item.price))" class="pr-2">Dyrest</span>
+                        <span v-if="isSameHour(item.datetime)" class="pr-2">Nu</span>
                     </div>
                 </div>
-                <div class="absolute border-t w-full border-blue-400/50 z-20 animate-pulse"
+                <div class="absolute h-4 w-full bg-blue-400/30 z-20 animate-pulse"
                     v-if="isSameHour(item.datetime)"></div>
-                <div class="absolute h-4 w-full z-20 opacity-20" :style="'background-color: ' + barColor(item.price)"
+                <div class="absolute h-4 w-full z-20 opacity-20" :style="`background-color: hsl(${barColorHue(item.price)} 60% 40%)`"
                     v-if="item.price === min(last36Hours.map(item => item.price))"></div>
-                <div class="absolute h-4 w-full z-20 opacity-20" :style="'background-color: ' + barColor(item.price)"
+                <div class="absolute h-4 w-full z-20 opacity-20" :style="`background-color: hsl(${barColorHue(item.price)} 60% 40%)`"
                     v-if="item.price === max(last36Hours.map(item => item.price))">
                 </div>
             </div>
@@ -53,6 +55,6 @@ onMounted(async () => {
 const min = (prices: number[]): number => Math.min(...prices)
 const max = (prices: number[]): number => Math.max(...prices)
 const isSameHour = (datetime: Date): boolean => dayjs().startOf('hour').isSame(dayjs(datetime).startOf('hour'))
-const barColor = (price: number): string => `hsl(${120 - (afgifter + price) / maxPrice.value * 120} 60% 40%)`
+const barColorHue = (price: number): number => (120 - (afgifter + price) / maxPrice.value * 120)
 const barLength = (price: number): number => (afgifter + price) / maxPrice.value * 90
 </script>
