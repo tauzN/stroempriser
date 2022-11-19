f2d0fb36be32a846a6b9835e8a1c99e3891659a5
Martin Tausen <martausen@gmail.com>
2022-11-19 11:55:23 +0100

    updated prices for nov. 2022

--- src/util/prices.ts
+++ src/util/prices.ts
@@ -1,7 +1,7 @@
 import dayjs from "dayjs";
 // Nettarif https://n1.dk/priser-og-vilkaar
-const lavlast: number = (.3153 - .0202); // DKK
-const spidslast: number = (.8216 - .0425); // DKK
+const lavlast: number = (.4077 - .0202); // DKK
+const spidslast: number = (1.10159 - .0425); // DKK
 // Elafgift https://www.skm.dk/skattetal/satser/satser-og-beloebsgraenser-i-lovgivningen/elafgiftsloven/
 const elafgift: number = .723; // DKK
 // Forbrugstariffer https://energinet.dk/El/Elmarkedet/Tariffer/Aktuelle-tariffer
@@ -15,11 +15,13 @@ const aarsforbrug = 1_500; // kWh
 const netabonnement: number = 290 / aarsforbrug; // DDK * øre/DKK /
 const flat_fee = elafgift + forbrugstariffer + netabonnement
 
-const maanederMedSpidslast =[0, 1, 2, 9, 10, 11]
-const timerMedSpidslast =  [17, 18, 19]
+const maanederMedSpidslast = [0, 1, 2, 9, 10, 11]
+const timerMedSpidslast = [17, 18, 19]
 export const afgift = (date: Date): number => {
   if (maanederMedSpidslast.includes(dayjs(date).month()) && timerMedSpidslast.includes(dayjs(date).hour()))
     return ((flat_fee + spidslast) * 1.25)
   else
     return ((flat_fee + lavlast) * 1.25)
 }
+export const afgifter_lavlast = () => (flat_fee + lavlast) * 1.25
+export const afgifter_spidslast = () => (flat_fee + spidslast) * 1.25
