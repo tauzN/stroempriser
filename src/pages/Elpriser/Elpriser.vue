9bd7d08b341f1dd800897634936ef0d4eac8c043
Martin Tausen <martausen@gmail.com>
2022-12-07 21:26:49 +0100

    add loading indicator

--- src/pages/Elpriser/Elpriser.vue
+++ src/pages/Elpriser/Elpriser.vue
@@ -1,11 +1,18 @@
 <template>
     <div class="text-gray-100 text-xs flex p-2 gap-2 justify-center flex-wrap lg:flex-nowrap mx-auto">
-        <Page3chart v-if="records && dayjs(records[0].datetime).isSame(dayjs(), 'day')" :records="records.slice(0,24)" :maxPrice="maxPrice">
+        <div class="animate-pulse text-lg" v-show="records === undefined">
+            Henter priser...
+        </div>
+        <Page3chart v-if="records && dayjs(records[0].datetime).isSame(dayjs(), 'day')" :records="records.slice(0, 24)"
+            :maxPrice="maxPrice">
         </Page3chart>
         <Page3chart v-if="records" :records="records.slice(24)" :maxPrice="maxPrice"></Page3chart>
     </div>
     <div class=" max-w-xs text-gray-400 text-xs mx-auto text-center">
-        Priser er ekskl. abbonement  til elselskab og spottillæg. Der er ingen nettarif eller netabonnement hos N1 i december <a target="_blank" class=" text-blue-500 hover:underline" href="https://via.ritzau.dk/pressemeddelelse/750000-jyske-elkunder-slipper-for-nettarif-i-december?publisherId=13559703&releaseId=13663280&lang=da">læs mere</a>
+        Priser er ekskl. abbonement til elselskab og spottillæg. Der er ingen nettarif eller netabonnement hos N1 i
+        december <a target="_blank" class=" text-blue-500 hover:underline"
+            href="https://via.ritzau.dk/pressemeddelelse/750000-jyske-elkunder-slipper-for-nettarif-i-december?publisherId=13559703&releaseId=13663280&lang=da">læs
+            mere</a>
 
     </div>
 </template>
@@ -15,9 +22,14 @@ import { computed, onMounted, ref } from "vue"
 import { record } from "../../types";
 import { getLastHours } from "../../util/util";
 import Page3chart from "./Chart.vue";
-const records = ref<record[]>()
+const records = ref<record[] | null>()
 onMounted(async () => {
-    records.value = await getLastHours(48)
+    try {
+        records.value = await getLastHours(48)
+    } catch (error) {
+        records.value = null
+        console.log(error);
+    }
 })
 const maxPrice = computed(() => {
     let max = 0;
