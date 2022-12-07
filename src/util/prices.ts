511b2c0f4425b065e3b88e162f4d28fd6202f4ee
Martin Tausen <martausen@gmail.com>
2022-12-07 21:26:40 +0100

    restructuring

--- src/util/prices.ts
+++ src/util/prices.ts
@@ -1,7 +1,5 @@
 import dayjs from "dayjs";
-// Nettarif https://n1.dk/priser-og-vilkaar
-const lavlast: number = 0//(.4077 - .0202); // DKK
-const spidslast: number = 0//(1.10159 - .0425); // DKK
+
 // Elafgift https://www.skm.dk/skattetal/satser/satser-og-beloebsgraenser-i-lovgivningen/elafgiftsloven/
 const elafgift: number = .723; // DKK
 // Forbrugstariffer https://energinet.dk/El/Elmarkedet/Tariffer/Aktuelle-tariffer
@@ -9,12 +7,14 @@ const transmissionsnettarif = .049
 const systemtarif = .061
 const balancetarif = .00229
 const forbrugstariffer = transmissionsnettarif + systemtarif + balancetarif
-// årsforbrug i kWh
-const aarsforbrug = 3000; // kWh
+const aarsforbrug = 3000; // årsforbrug i kWh
 // Netabonnement (net) 290 kr. pr. år
 const netabonnement: number = 0 // 290 / aarsforbrug; // DDK * øre/DKK /
 const flat_fee = elafgift + forbrugstariffer + netabonnement
 
+// Nettarif https://n1.dk/priser-og-vilkaar
+const lavlast: number = 0//(.4077 - .0202); // DKK
+const spidslast: number = 0//(1.10159 - .0425); // DKK
 const maanederMedSpidslast = [0, 1, 2, 9, 10, 11]
 const timerMedSpidslast = [17, 18, 19]
 export const afgift = (date: Date): number => {
