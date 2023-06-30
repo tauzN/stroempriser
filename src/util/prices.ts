ddfc354af23632ef5a3cc3a69c09eebcf3d1ae7c
Martin Tausen <martausen@gmail.com>
2023-06-30 19:18:45 +0200

    update price for july 23

--- src/util/prices.ts
+++ src/util/prices.ts
@@ -1,7 +1,7 @@
 import dayjs from "dayjs";
 
 // Elafgift https://www.skm.dk/skattetal/satser/satser-og-beloebsgraenser-i-lovgivningen/elafgiftsloven/
-const elafgift: number = .008; // DKK
+const elafgift: number = .697; // DKK
 // Forbrugstariffer https://energinet.dk/El/Elmarkedet/Tariffer/Aktuelle-tariffer
 const transmissionsnettarif = .058
 const systemtarif = .054
@@ -12,7 +12,7 @@ const flat_fee = elafgift + forbrugstariffer
 // Nettarif https://n1.dk/priser-og-vilkaar
 const lavlast: number = 0.3268 // DKK
 const spidslast: number = .08409 // DKK
-const netabn = 283/12/300 // DKK/år/kwh
+const netabn = 283/12/300 // DKK/mdr/kwh/mdr
 const maanederMedSpidslast = [0, 1, 2, 9, 10, 11]
 const timerMedSpidslast = [17, 18, 19]
 export const afgiftFromDate = (date: Date): number => {  
--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,7 @@
-import { defineConfig, loadEnv } from 'vite'
+import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import fs from 'fs'
 
 // https://vitejs.dev/config/
-export default ({ mode }) => {
-  //console.log(process.env);
-  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
-  return defineConfig({
-    plugins: [vue()],
-    server: {
-      host: process.env.VITE_LOCAL_HOST,
-      port: 3000,
-      https: process.env.NODE_ENV === "production" ? {} : {
-        key: fs.readFileSync("../" + process.env.VITE_LOCAL_HOST + "-key.pem"),
-        cert: fs.readFileSync("../" + process.env.VITE_LOCAL_HOST + ".pem")
-      }
-    }
-  })
-}
+export default defineConfig({
+  plugins: [vue()],
+})
