9fa23955ac360a6ad7b73200d698826f02862ac0
Martin Tausen <martausen@gmail.com>
2023-10-29 14:24:17 +0100

    updated expected usage, more pransparent prices

--- src/util/prices.ts
+++ src/util/prices.ts
@@ -1,18 +1,20 @@
 import dayjs from "dayjs";
 
-// Elafgift https://www.skm.dk/skattetal/satser/satser-og-beloebsgraenser-i-lovgivningen/elafgiftsloven/
-export const elafgift: number = .697; // DKK
 // Forbrugstariffer https://energinet.dk/El/Elmarkedet/Tariffer/Aktuelle-tariffer
-const transmissionsnettarif = .058
-const systemtarif = .054
-const balancetarif = 0
+const transmissionsnettarif = 5.8 / 100 // DKK
+const systemtarif =  5.4 / 100 // DKK
+const balancetarif = 0 / 100 // DKK
 export const forbrugstariffer = transmissionsnettarif + systemtarif + balancetarif
+
+// Elafgift https://www.skm.dk/skattetal/satser/satser-og-beloebsgraenser-i-lovgivningen/elafgiftsloven/
+export const elafgift: number =  68.7 / 100; // DKK
+
 const flat_fee = elafgift + forbrugstariffer
 
 // Nettarif https://n1.dk/priser-og-vilkaar
-const lavlast: number = 0 //0.3268 // DKK
-const spidslast: number = 0 //.08409 // DKK
-const netabn = 283/12/300 // DKK/mdr/kwh/mdr
+const lavlast: number = 0 // DKK
+const spidslast: number = 0 // DKK
+const netabn = 283/12/400 // DKK/mdr/kwh/mdr
 const maanederMedSpidslast = [0, 1, 2, 9, 10, 11]
 const timerMedSpidslast = [17, 18, 19]
 export const afgiftFromDate = (date: Date): number => {  
