8a5898c6b7598b24923dc86bbd8118b1912ecda9
Martin Tausen <martausen@gmail.com>
2023-10-29 14:44:03 +0100

    nav links

--- src/Router.vue
+++ src/Router.vue
@@ -4,10 +4,10 @@
       <component :is="Component" />
     </KeepAlive>
   </router-view>
-  <div class="fixed bottom-0 left-0 z-50 flex items-center w-full h-10 text-xs bg-gray-800 justify-evenly">
+  <nav class="fixed bottom-0 left-0 z-50 flex items-center w-full h-10 text-xs bg-gray-800 justify-evenly">
     <router-link to="/">Elpriser</router-link>
     <router-link to="/elbil">Opladning</router-link>
-  </div>
+  </nav>
   <div class="h-10"></div>
 </template>
 <style scoped>
