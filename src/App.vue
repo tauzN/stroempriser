30d236e3b1705181bc663c6b4a3b92fa2cd95afa
Martin Tausen <martausen@gmail.com>
2024-01-23 23:37:06 +0100

    layout, nav

--- src/App.vue
+++ src/App.vue
@@ -2,13 +2,22 @@
 </script>
 
 <template>
-  <div></div>
-  <RouterView></RouterView>
-  <div>
-    <RouterLink to="/">Elpriser</RouterLink>
-    <RouterLink to="/elbil">ELbil</RouterLink>
+  <div class="flex flex-col ">
+    <div class="absolute top-0 w-screen overflow-y-scroll bottom-10">
+      <RouterView></RouterView>
+    </div>
+    <nav class="fixed bottom-0 left-0 flex justify-around w-screen h-10 bg-gray-900">
+      <RouterLink to="/">Elpriser</RouterLink>
+      <RouterLink to="/elbil">Opladning</RouterLink>
+    </nav>
   </div>
 </template>
 
 <style scoped>
+nav a {
+  @apply px-4 w-full text-center flex items-center justify-center
+}
+.router-link-active {
+  @apply bg-gray-700
+}
 </style>
