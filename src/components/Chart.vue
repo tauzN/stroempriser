9e7d319457434a46a6c4a5212da8f29fc32632c7
Martin Tausen <martausen@gmail.com>
2024-06-24 13:27:39 +0200

    colors

--- src/components/Chart.vue
+++ src/components/Chart.vue
@@ -1,25 +1,24 @@
 <template>
     <div class="flex flex-col p-4 border border-gray-700 rounded-lg ">
         <div class="flex justify-between ">
-            <div
-                class="pb-2 font-semibold tracking-wider text-gray-300 uppercase text-md">
+            <div class="pb-2 font-semibold tracking-wider text-gray-300 uppercase text-md">
                 {{
                     dayjs(props.records[0].datetime).format("dddd") }}</div>
-            <div>{{ (Math.min(...records.map(r => r.price + afgiftFromDate(r.datetime))) *1.25).toFixed(2).replace(".", ",")}}</div>
-            <div>{{ (Math.max(...records.map(r => r.price + afgiftFromDate(r.datetime))) * 1.25).toFixed(2).replace(".", ",") }}</div>
+            <div>{{ (Math.min(...records.map(r => r.price + afgiftFromDate(r.datetime))) * 1.25).toFixed(2).replace(".",
+                    ",") }}</div>
+            <div>{{ (Math.max(...records.map(r => r.price + afgiftFromDate(r.datetime))) * 1.25).toFixed(2).replace(".",
+                    ",") }}</div>
         </div>
         <div class="flex gap-0.5">
-            <div class="flex flex-col items-center justify-end "
-                v-for="record in records">
+            <div class="flex flex-col items-center justify-end " v-for="record in records">
                 <div class="pb-2 font-mono text-[0.5rem] text-gray-300 text-vertical "
                     :class="{ 'font-bold': dayjs(record.datetime).isSame(dayjs(), 'hour') }">
                     {{ ((afgiftFromDate(record.datetime) + record.price) *
-                        1.25).toFixed(2).replace(".", ",") }}
+                    1.25).toFixed(2).replace(".", ",") }}
                 </div>
-                <div class="flex flex-col justify-end w-2 overflow-hidden rounded-full h-36"
-                    :class="{
-                        'bg-blue-800 animate-pulse': dayjs(record.datetime).isSame(dayjs(), 'hour') , 'bg-gray-900': !dayjs(record.datetime).isSame(dayjs(), 'hour')
-                        }">
+                <div class="flex flex-col justify-end w-2 overflow-hidden rounded-full h-36" :class="{
+                    'bg-blue-800 animate-pulse': dayjs(record.datetime).isSame(dayjs(), 'hour'), 'bg-gray-900': !dayjs(record.datetime).isSame(dayjs(), 'hour')
+                }">
                     <div class="rounded-t-full"
                         :style="`height: ${height(record)}%; background-color: hsl(${barColorHue(record)}, 100%, 30%)`">
                     </div>
@@ -47,14 +46,14 @@ const height = (record: Record): number => (afgiftFromDate(record.datetime) + re
 
 
 const barColorHue = (record: Record): number => {
-    if ((afgiftFromDate(record.datetime) + record.price) > props.maxPrice) {
+    if ((afgiftFromDate(record.datetime) + record.price) > props.maxPrice - .5) {
         return 0
     }
     else if ((afgiftFromDate(record.datetime) + record.price) < 1.1) {
         return 120
     }
     else {
-        return 120 - 30 * (afgiftFromDate(record.datetime) + record.price)
+        return 120 - 40 * (afgiftFromDate(record.datetime) + record.price)
     }
 }
 
